59c5ce20b28adc9b3b52563b64a2ab67ca0a8919
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-28 14:32:11 -0300

    feat: adicionar chave secreta do token JWT

---
+++ src/configs/auth.ts
@@ -0,0 +1,6 @@
+export const authConfig = {
+    jwt: {
+        secret: "yuri",
+        expiresIn: 60 * 60 * 24
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { userRoute } from "./user-route";
+import { sessionsRoute } from "./sessions-routes";
 
 const route = Router()
 
 route.use("/users", userRoute)
+route.use("/sessions", sessionsRoute)
 
 export { route }
