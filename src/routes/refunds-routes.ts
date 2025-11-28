1563dbae7924b7e2b62f9c5c0023976dc3b21a9e
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-28 15:09:05 -0300

    feat: adicionar autorização do usuário

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { RefundsController } from "@/controllers/RefundsController";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const refundsRoutes = Router()
 const refundsController = new RefundsController()
 
-refundsRoutes.post("/", refundsController.create)
+refundsRoutes.post("/", verifyUserAuthorization(["employee"]), refundsController.create)
 
 export { refundsRoutes }
