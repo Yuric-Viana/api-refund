8c41a5aea47411608431a2f69aaa90f8cf3623b4
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-01 14:26:37 -0300

    feat: adicionar controlle de criação e de leitura das solicitações para reembolso

--- src/controllers/RefundsController.ts
+++ src/controllers/RefundsController.ts
@@ -1,8 +1,98 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { Category } from "@prisma/client";
+import { z } from 'zod'
+import { AppError } from "@/utils/AppError";
 
 class RefundsController {
     async create(req: Request, res: Response) {
-        return res.json({ message: "OK" })
+        const bodySchema = z.object({
+            name: z.string().trim().min(3, { message: "O nome é obrigatório." }),
+            category: z.enum([Category.food, Category.services, Category.others, Category.transport, Category.accomodation]),
+            amount: z.number().positive({ message: "O número precisa ser positivo." }),
+            filename: z.string().min(20)
+        })
+
+        const { name, amount, category, filename } = bodySchema.parse(req.body)
+
+        const refunds = await prisma.refunds.create({
+            data: {
+                name,
+                category,
+                amount,
+                filename,
+                userId: req.user.id
+            }
+        })
+
+        return res.json(refunds)
+    }
+
+    async index(req: Request, res: Response) {
+        const querySchema = z.object({
+            name: z.string().optional().default(""),
+            page: z.coerce.number().optional().default(1),
+            perPage: z.coerce.number().optional().default(10),
+        })
+
+        const { name, page, perPage } = querySchema.parse(req.query)
+
+        const skip = (page - 1) * perPage
+
+        const refunds = await prisma.refunds.findMany({
+            skip,
+            take: perPage,
+            where: {
+                user: {
+                    name: {
+                        contains: name.trim()
+                    }
+                }
+            },
+            orderBy: { createdAt: "desc" },
+            include: { user: true }
+        })
+
+        const totalRecords = await prisma.refunds.count({
+            where: {
+                user: {
+                    name: {
+                        contains: name.trim()
+                    }
+                }
+            },
+        })
+
+        const totalPages = Math.ceil(totalRecords / perPage)
+
+        return res.json({
+            refunds,
+            pagination: {
+                page,
+                perPage,
+                totalRecords,
+                totalPages: totalPages > 0 ? totalPages : 1
+            }
+        })
+    }
+
+    async show(req: Request, res: Response) {
+        const paramsSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = paramsSchema.parse(req.params)
+
+        const refund = await prisma.refunds.findFirst({
+            where: {
+                id
+            },
+            include: {
+                user: true
+            }
+        })
+
+        return res.json(refund)
     }
 }
 
--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -6,6 +6,8 @@ import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization
 const refundsRoutes = Router()
 const refundsController = new RefundsController()
 
-refundsRoutes.post("/", verifyUserAuthorization(["employee"]), refundsController.create)
+refundsRoutes.post("/create-refunds", verifyUserAuthorization(["employee"]), refundsController.create)
+refundsRoutes.get("/", verifyUserAuthorization(["manager"]), refundsController.index)
+refundsRoutes.get("/:id", verifyUserAuthorization(["manager", "employee"]), refundsController.show)
 
 export { refundsRoutes }
