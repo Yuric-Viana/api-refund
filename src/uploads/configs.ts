95f020a451ecf537f35f01cca16374936fb5431b
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-01 14:29:31 -0300

    feat: adicionar configuração do multer para uploads de arquivos

---
+++ src/uploads/configs.ts
@@ -0,0 +1,32 @@
+import multer from "multer";
+import path from "node:path"
+import crypto from "node:crypto"
+
+// Essa é uma pasta temporária onde o arquivo ficará permitindo a manipulação desse arquivo antes de levá-lo para a pasta de uploads
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
+
+const UPLOAD_FOLDER = path.relative(TMP_FOLDER, "uploads")
+
+const MAX_SIZE_FOLDER = 1024 * 1024 * 3
+
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
+
+const MULTER = {
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString("hex")
+            const fileName = `${fileHash}-${file.originalname}`
+
+            return callback(null, fileName)
+        }
+    })
+}
+
+export {
+    TMP_FOLDER,
+    UPLOAD_FOLDER,
+    MAX_SIZE_FOLDER,
+    ACCEPTED_IMAGE_TYPES,
+    MULTER
+}
